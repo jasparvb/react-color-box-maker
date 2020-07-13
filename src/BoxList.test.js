11545e0f86b35b6e7dd8d105712b74c973af3a34
von Buelow <jaspar@UMICH.EDU>
2020-07-13 17:45:47 -0400

    Tests for BoxList

--- src/BoxList.test.js
+++ src/BoxList.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import BoxList from './BoxList';
 
 // smoke test
@@ -10,4 +10,53 @@ it("renders without crashing", function() {
 it("matches snapshot", function() {
   const {asFragment} = render(<BoxList />);
   expect(asFragment()).toMatchSnapshot();
+});
+
+
+it("can add a new box", function() {
+  const { getByLabelText, queryByText, getByText } = render(<BoxList />);
+
+  // no items yet
+  expect(queryByText("X")).not.toBeInTheDocument();
+
+  const widthInput = getByLabelText("Width:");
+  const heightInput = getByLabelText("Height:");
+  const colorInput = getByLabelText("Background Color:");
+  const submitBtn = queryByText("Add a new box!")
+
+  // fill out the form
+  fireEvent.change(widthInput, { target: { value: "200px" }});
+  fireEvent.change(heightInput, { target: { value: "100px" }});
+  fireEvent.change(colorInput, { target: { value: "#b13e3e" }});
+  fireEvent.click(submitBtn);
+
+  // item exists!
+  const removeBtn = getByText("X");
+  expect(removeBtn).toBeInTheDocument();
+  expect(removeBtn.previousSibling).toHaveStyle(`
+    width: 200px;
+    height: 100px;
+    background-color: #b13e3e;
+  `);
+
+});
+
+it("can remove a new box", function() {
+  const { getByLabelText, queryByText, getByText } = render(<BoxList />);
+
+  const widthInput = getByLabelText("Width:");
+  const heightInput = getByLabelText("Height:");
+  const colorInput = getByLabelText("Background Color:");
+  const submitBtn = queryByText("Add a new box!")
+
+  // fill out the form
+  fireEvent.change(widthInput, { target: { value: "200px" }});
+  fireEvent.change(heightInput, { target: { value: "100px" }});
+  fireEvent.change(colorInput, { target: { value: "#b13e3e" }});
+  fireEvent.click(submitBtn);
+  
+  // item exists!
+  const removeBtn = getByText("X");
+  fireEvent.click(removeBtn);
+  expect(removeBtn).not.toBeInTheDocument();
 });
