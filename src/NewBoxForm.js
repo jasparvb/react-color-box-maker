35cd8ab31ffd63a891bf19741067be07343f48f9
von Buelow <jaspar@UMICH.EDU>
2020-07-13 05:24:33 -0400

    NewBoxForm component

--- src/NewBoxForm.js
+++ src/NewBoxForm.js
@@ -1,1 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
+
+const NewBoxForm = ({ addBox }) => {
+    const INITIAL_STATE = { width: "", height: "", backgroundColor: "" };
+    const [formData, setFormData] = useState(INITIAL_STATE);
+  
+    /** Send {width, height, backgroundColor} to parent
+     *    & clear form. */
+  
+    const handleSubmit = evt => {
+      evt.preventDefault();
+      addBox(formData);
+      setFormData(INITIAL_STATE);
+    };
+  
+    /** Update local state w/curr state of input elem */
+  
+    const handleChange = evt => {
+      const { name, value } = evt.target;
+      setFormData(fData => ({
+        ...fData,
+        [name]: value
+      }));
+    };
+  
+    /** render form */
+  
+    return (
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="width">Width:</label>
+        <input
+          id="width"
+          name="width"
+          value={formData.width}
+          onChange={handleChange}
+        />
+
+        <label htmlFor="height">Height:</label>
+        <input
+          id="height"
+          name="height"
+          value={formData.height}
+          onChange={handleChange}
+        />
+  
+        <label htmlFor="backgroundColor">Height:</label>
+        <input
+          type="color"
+          id="backgroundColor"
+          name="backgroundColor"
+          value={formData.backgroundColor}
+          onChange={handleChange}
+        />
+  
+        <button>Add a new box!</button>
+      </form>
+    );
+  };
+  
+  export default NewBoxForm;
+  
